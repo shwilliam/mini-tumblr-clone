8b8f7cfce988757bad7e04ee9922c5514ec6eab9
William L <w-lindvall@outlook.com>
2019-06-07 01:35:24 -0700

    dumb filter

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,3 +1,17 @@
-const feed = (root, args, context) => context.prisma.posts()
+const feed = async (root, args, context) => {
+  const where = args.filter
+    ? {
+        OR: [
+          {description_contains: args.filter},
+          {imgUrl_contains: args.filter},
+        ],
+      }
+    : {}
+
+  const posts = await context.prisma.posts({
+    where,
+  })
+  return posts
+}
 
 module.exports = {feed}
